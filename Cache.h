9f6b0bf07d19809c7110c880698f872051e19517
RahulVarma <rahul1995varma@users.noreply.github.com>
2020-08-21 09:25:09 -0400

    Add files via upload

---
+++ Cache.h
@@ -0,0 +1,208 @@
+typedef unsigned long ulong;
+using namespace std;
+
+string  REPLACEMENT_POLICY;
+string  INCLUSION_TYPE;
+bool ISL2;
+bool EXCLUSIVITY;
+
+class Cache {
+ private:
+  ulong size, assoc, blockSize, numberOfSets, blockOffset, reads, writes, readMisses, writeMisses, writeBacks,  evictedAddress, loopComp;
+  string missType;
+  bool writeBack, evicted;
+  class CacheBlock*** CacheBlock;
+  Cache* lowerCache;
+
+ public: 
+  ulong RPSeqNo; //Replacement Policy
+  ulong* Set;
+
+  Cache(ulong blockSize, ulong assoc, ulong size) {
+    reads = writes = readMisses = writeMisses = writeBacks = loopComp = 0;
+    this->RPSeqNo = 0;
+    this->size = size;
+    this->assoc = assoc;
+    this->blockSize = blockSize;
+    this->numberOfSets = size/(blockSize * assoc); 
+    this->blockOffset = (ulong) log2(blockSize);
+    Set = new ulong[numberOfSets];
+    this->CacheBlock = new class CacheBlock**[numberOfSets];
+    for (int i=0; i < (int) numberOfSets; ++i){
+      CacheBlock[i] = new class CacheBlock*[assoc];
+      for (int j=0; j < (int) assoc; ++j) {
+	loopComp++;
+	CacheBlock[i][j] = new class CacheBlock(assoc);
+      }
+      Set[i] = 0;
+    } 
+    this->lowerCache = NULL;
+  }
+
+  void initL2(Cache* cache) {
+    this->lowerCache = cache;
+  }
+
+  // Getters and setters for Private variables
+
+  ulong getEvictedAddress() { return this->evictedAddress; }
+
+  void setEvictedAddress(ulong tag) { if(this->evicted) this->evictedAddress = getAddress(tag); }
+
+  void setWriteBack(bool cond) { this->writeBack = cond; }
+
+  void setEvicted(bool cond) { this->evicted = cond; }
+
+  void incWriteBacks(){ this->writeBacks++; }
+
+  void incReads() { this->reads++; }
+
+  void incWrites() { this->writes++; }
+  
+  void addReadMiss() { this->readMisses++;  this->missType = "READMISS"; }
+
+  void addWriteMiss() { this->writeMisses++;  this->missType = "WRITEMISS"; }
+
+  ulong getMemoryTraffic() { return(this->readMisses + this->writeMisses + this->writeBacks); }
+
+  float getMissRate() { return(((float)(this->readMisses + this->writeMisses))/((float)(this->reads + this->writes))); } 
+
+  ulong getReads() { return this->reads; }
+
+  ulong getWrites() { return this->writes; }
+
+  ulong getReadMisses() { return this->readMisses; }
+
+  ulong getWriteMisses() { return this->writeMisses; }
+
+  ulong getWriteBacks() const { return this->writeBacks; } 
+
+  Cache * getLowerCache() { return this->lowerCache; }
+
+  // checking bool conditions
+  
+  bool isWriteBack() { return this->writeBack; }
+
+  bool isEvicted() { return this->evicted; }
+
+  bool isMiss() { return (this->missType == "READMISS" || this->missType == "WRITEMISS"); }
+
+  bool isHit() { return (this->missType == "HIT"); }
+
+  // Bit manipulations and calculations
+
+  ulong getAddress(ulong tag) { return (tag << blockOffset); }
+
+  ulong getIndex(ulong address) { return (getTag(address) & (ulong)(numberOfSets - 1)); }
+
+  ulong getTag(ulong address) { return (address >> (blockOffset)); }
+  
+  // Try to access the block
+  
+  void getBlockAccess(ulong address, char method){
+    missType = "UNKNOWN";
+    setWriteBack(false);
+    setEvicted(false);
+    
+    class CacheBlock* CacheBlock = findCacheBlock(address);
+    if(method == 'r') incReads();  else incWrites(); 
+      
+    if(CacheBlock == NULL) {
+      if (method == 'r') addReadMiss();  else addWriteMiss();
+      if(EXCLUSIVITY)  CacheBlock = fillCacheBlock(address);
+    }
+    else {
+      missType = "HIT";
+      if (REPLACEMENT_POLICY != "FIFO")  processLRU(CacheBlock);
+    } 
+    if (method == 'w')
+      CacheBlock->setState("DIRTY"); 
+  }
+
+  // Find the block
+
+  class CacheBlock* findCacheBlock(ulong address){
+    ulong tag = getTag(address);
+    class CacheBlock** CacheBlock = this->CacheBlock[getIndex(address)];
+    for(int i=0; i < (int) assoc; ++i) {
+      loopComp++;
+      if((tag == CacheBlock[i]->getTag()) && CacheBlock[i]->isValid())
+	return CacheBlock[i]; 
+    }
+    return NULL;
+  }
+
+  // Get The Block
+  class CacheBlock* getBlockToBeFilled(ulong address) {
+    class CacheBlock* blockToBeFilled = getInvalidBlock(getIndex(address));
+    if (!blockToBeFilled) {
+      setEvicted(true);
+      blockToBeFilled = getRepPolBlock(getIndex(address)); 
+    } 
+    return blockToBeFilled;
+  }
+
+
+  // Check the invalid/empty block
+  class CacheBlock* getInvalidBlock(ulong index){
+    for (int i=0; i < (int) assoc; ++i) {
+      loopComp++;
+      if(!CacheBlock[index][i]->isValid())
+	return CacheBlock[index][i]; 
+    }
+    return NULL;
+  }
+
+
+  class CacheBlock* getRepPolBlock(ulong index){
+    class CacheBlock* block = CacheBlock[index][0];
+    for (int i=1; i < (int) assoc; ++i){
+      loopComp++;
+      if(CacheBlock[index][i]->getSeq() < block->getSeq()){
+	block = CacheBlock[index][i];
+      }
+    }
+    return block;
+  }
+
+  // Fill The Block
+  
+  class CacheBlock* fillCacheBlock(ulong address){
+    int index = getIndex(address);
+    class CacheBlock* blockToBeFilled = getBlockToBeFilled(address);
+    setEvictedAddress(blockToBeFilled->getTag());
+
+    if ((blockToBeFilled != NULL) && (blockToBeFilled->isDirty())) {
+      setWriteBack(true);
+      this->incWriteBacks();
+    }
+    if (blockToBeFilled->isValid()){ Set[index] = blockToBeFilled->getSeq(); }
+
+    blockToBeFilled->setTag(getTag(address));
+    blockToBeFilled->setState("VALID");
+    processLRU(blockToBeFilled);
+    return blockToBeFilled; 
+  }
+
+  // LRU Replacement Policy
+  void processLRU(class CacheBlock* cacheBlock) {
+    cacheBlock->setSeq(++RPSeqNo);
+  }
+  
+  // Invalidate the block
+  
+  void invalidateBlock(ulong address){
+    class CacheBlock* cacheBlock = findCacheBlock(address);
+    if(cacheBlock != NULL) {
+      if(INCLUSION_TYPE == "INCLUSION" && lowerCache){
+	if (cacheBlock->isDirty()){
+	  setWriteBack(true);
+	  this->incWriteBacks();
+	  lowerCache->setWriteBack(true);
+	  lowerCache->incWriteBacks();
+	}
+      }
+      cacheBlock->setState("INVALID");
+    } 
+  }
+};
---
+++ sim_cache.cc
@@ -0,0 +1,189 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<math.h>
+#include<bitset>
+#include<iostream>
+#include<string.h>
+#include "CacheBlock.h"
+#include "Cache.h"
+using namespace std;
+
+typedef unsigned long ulong;
+
+//Functions assisting Main 
+
+void printCacheConfig(ulong blockSize, ulong l1Size, ulong l1Assoc, ulong l2Size, ulong l2Assoc,char *fname ){
+  cout << "===== Simulator Configuration =====" << endl;
+  cout << "BLOCKSIZE: \t\t" << blockSize << endl;
+  cout << "L1_SIZE: \t\t" << l1Size << endl;
+  cout << "L1_ASSOC: \t\t" << l1Assoc << endl;
+  cout << "L2_SIZE: \t\t" << l2Size << endl;
+  cout << "L2_ASSOC: \t\t" << l2Assoc << endl;
+  cout << "REPLACEMENT POLICY: \t" << REPLACEMENT_POLICY << endl;
+  cout << "INCLUSION PROPERTY: \t" << INCLUSION_TYPE << endl;
+  cout << "trace_file: \t\t" << fname;
+}
+
+// Read Command Inputs
+
+bool readCmdInput(int argc, char *argv[],ulong *blockSize,ulong *l1Size,ulong *l1Assoc, ulong *l2Size, ulong *l2Assoc, FILE ** traceFile){
+  char * fname; 
+  if(argc <= 8) {
+    cout << "Not enough input parameters are provided" << endl;
+    cout << "Valid Input: .\\sim_cache <BLOCKSIZE> <L1_SIZE> <L1_ASSOC> <L2_SIZE> <L2_ASSOC> <REPL_POLICY> <INCLUSION> <TRACE_FILE> " << endl;
+    return false;
+  }
+  
+  *blockSize = atoi(argv[1]);
+  *l1Size = atoi(argv[2]);
+  *l1Assoc = atoi(argv[3]);
+  *l2Size = atoi(argv[4]);
+  *l2Assoc = atoi(argv[5]);
+
+  switch(atoi(argv[6])) {
+  case 0: REPLACEMENT_POLICY = "LRU";  break;
+  case 1: REPLACEMENT_POLICY = "FIFO";  break;
+  default: cout << "Wrong Replacement Policy: Valid Options: 0: LRU, 1: FIFO" << endl;  return false; 
+  }
+  
+  switch(atoi(argv[7])){
+  case 0: INCLUSION_TYPE = "NON-INCLUSIVE";  break;
+  case 1: INCLUSION_TYPE = "INCLUSIVE";  break;
+  case 2: INCLUSION_TYPE = "EXCLUSIVE";  break;
+  default: cout << "Wrong Inclusion Type: Valid Options 0: NON-INCLUSIVE, 1: INCLUSIVE, 2: EXCLUSIVE" << endl;  return false;
+  }
+
+  fname = argv[8]; 
+  *traceFile = fopen(fname, "r"); 
+  if(*traceFile == 0) {
+    cout << "\nThere is a problem reading the trace file." <<endl;
+    return false;
+  }
+
+  ISL2 = (*l2Size == 0) ? false : true;
+  printCacheConfig(*blockSize,*l1Size,*l1Assoc,*l2Size,*l2Assoc,fname);
+  return true;
+}
+
+// Output
+
+void displayOutput(Cache * L1){
+  cout << "\n===== Simulation Results (raw) =====" << endl;
+  cout << "a. number of L1 reads: \t\t" << L1->getReads() << endl;
+  cout << "b. number of L1 read misses: \t" << L1->getReadMisses() << endl;
+  cout << "c. number of L1 writes: \t" << L1->getWrites() << endl;
+  cout << "d. number of L1 write misses: \t" << L1->getWriteMisses() << endl;
+  printf("e. L1 miss rate: \t\t%.6f\n",L1->getMissRate());
+  cout << "f. number of L1 writebacks: \t" << L1->getWriteBacks() << endl; 
+  Cache * L2 = L1->getLowerCache();
+  if(L2) {
+    cout << "g. number of L2 reads: \t\t" << L2->getReads() << endl;
+    cout << "h. number of L2 read misses: \t" << L2->getReadMisses() << endl;
+    cout << "i. number of L2 writes: \t" << L2->getWrites() << endl;
+    cout << "j. number of L2 write misses: \t" << L2->getWriteMisses() << endl;
+    printf("k. L2 miss rate: \t\t%.6f\n",L2->getMissRate());
+    cout << "l. number of L2 writebacks: \t" << L2->getWriteBacks() << endl;
+    cout << "m. total memory traffic: \t" << L2->getMemoryTraffic() << endl;
+  } else {
+    cout << "g. number of L2 reads: \t\t0" << endl;
+    cout << "h. number of L2 read misses: \t0" << endl;
+    cout << "i. number of L2 writes: \t0"<< endl;
+    cout << "j. number of L2 write misses: \t0" << endl;
+    cout << "k. L2 miss rate: \t\t0" << endl;
+    cout << "l. number of L2 writebacks: \t0" << endl;
+    cout << "m. total memory traffic: \t" << L1->getMemoryTraffic() << endl;
+  }
+}
+
+// Access L1
+void accessL1(Cache * L1, char instr, ulong address) {
+  EXCLUSIVITY = true;
+  L1->getBlockAccess(address,instr); 
+}
+
+// Non-Inlcusive Cache
+void nonInclusiveCache(Cache * L1, Cache * L2, ulong address) {
+  EXCLUSIVITY = true;
+  if (L1->isWriteBack())  L2->getBlockAccess(L1->getEvictedAddress(), 'w'); 
+  if(L1->isMiss())  L2->getBlockAccess(address, 'r'); 
+}
+
+// Inclusive Cache
+void inclusiveCache(Cache * L1, Cache * L2, ulong address) {
+  EXCLUSIVITY = true; 
+  if (L1->isWriteBack()) {
+    L2->getBlockAccess(L1->getEvictedAddress(), 'w'); 
+    if (L2->isEvicted()) {
+      L1->invalidateBlock(L2->getEvictedAddress());
+    }
+  }
+  if(L1->isMiss()) {
+    L2->getBlockAccess(address, 'r');
+    if (L2->isEvicted())  L1->invalidateBlock(L2->getEvictedAddress());
+  }
+}
+
+// Exclusive Cache
+void exclusiveCache(Cache * L1, Cache * L2, ulong address) {
+  EXCLUSIVITY = true;
+  if(L1->isHit()) L2->invalidateBlock(address);
+  if(L1->isEvicted()) {
+    L2->getBlockAccess(L1->getEvictedAddress(),'w');
+    if(!L1->isWriteBack()) L2->findCacheBlock(L1->getEvictedAddress())->setState("VALID"); 
+  }
+  if(L1->isMiss()) {
+    EXCLUSIVITY = false;
+    L2->getBlockAccess(address,'r');
+    EXCLUSIVITY = true;
+    if(L2->isHit()){
+      if(L2->findCacheBlock(address)->isDirty())  L1->findCacheBlock(address)->setState("DIRTY");
+	L2->invalidateBlock(address);
+      }
+    } 
+  }
+
+  // Main Starts Here
+
+  int main(int argc, char *argv[]){
+
+    bool isConfigSuccessful; 
+    ulong blockSize, l1Size, l1Assoc, l2Size, l2Assoc, address;
+    char *token, *method;
+    FILE *traceFile;
+    char instruction[20];
+    Cache * L1, * L2;
+  
+    isConfigSuccessful = readCmdInput(argc,argv,&blockSize,&l1Size,&l1Assoc,&l2Size,&l2Assoc,&traceFile);
+
+    if(!isConfigSuccessful)  return 0;
+  
+    // initialize caches
+  
+    L1 = new Cache(blockSize, l1Assoc, l1Size ); 
+    if(ISL2){
+      L2 = new Cache(blockSize, l2Assoc, l2Size);
+      L1->initL2(L2);
+    } 
+
+    // Read the trace file and simulate cache config
+
+    while(fgets(instruction,20,traceFile) != NULL) {
+      token = strtok(instruction, " "); 
+      method = token;
+      token = strtok(NULL," ");
+      address = strtol(token,NULL,16);
+
+      accessL1(L1, method[0], address);
+
+      if(ISL2)
+	switch(atoi(argv[7])){
+	case 0: nonInclusiveCache(L1, L2, address); break;
+	case 1: inclusiveCache(L1, L2, address); break;
+	case 2: exclusiveCache(L1, L2, address); break; 
+	}
+    }
+  
+    displayOutput(L1);
+    return 1;
+  }
